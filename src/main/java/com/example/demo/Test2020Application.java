13a4ae38fd0e92418fda89894063c5a50d9c89f4
zhen_zhang <17321129370@163.com>
2019-11-18 13:16:17 +0800

    新建自动化项目

---
+++ src/main/java/com/example/demo/Test2020Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test2020Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test2020Application.class, args);
+    }
+
+}
